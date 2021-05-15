1e0e1fe6f846198a0a78511ac9008bff33ad876c
Mrklata <lukasz98kowalczyk@gmail.com>
2021-05-15 18:27:02 +0200

    Adding train_test_split

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import sklearn
 import os
 
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_wine
 from sklearn.dummy import DummyClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -11,16 +12,21 @@ from joblib import Memory
 wine = load_wine()
 features, target = wine.data, wine.target
 
+X_train, X_test, y_train, y_test = train_test_split(features, target)
+
 dummy_clf = DummyClassifier()
 
 # cache_dir = os.path.join('cache')
 # memory = Memory(cache_dir)
 
 dummy_clf.fit(features, target)
-print(dummy_clf.score(features, target))
+
+print(f'dummy TEST score: {dummy_clf.score(X_test, y_test)}')
+print(f'dummy TRAIN score: {dummy_clf.score(X_train, y_train)}')
 
 
 for i in range(1, 20):
     kneighbor_clf = KNeighborsClassifier(n_neighbors=i)
     kneighbor_clf.fit(features, target)
-    print(kneighbor_clf.score(features, target))
+    print(f'Kneighbor TEST score for {i} neighbors: {kneighbor_clf.score(X_test, y_test)}')
+    print(f'Kneighbor TRAIN score for {i} neighbors: {kneighbor_clf.score(X_train, y_train)}')
